4b79f194402adb9afea341f512fefdcb9be3c5de
Ren Tatsumoto <tatsu@autistici.org>
2026-03-01 03:01:56 +0300

    format
    add ocr icon path
    
    resolve thid dir

--- lancet/__main__.py
+++ lancet/__main__.py
@@ -1,15 +1,13 @@
-"""
-Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
-License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-"""
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import sys
 
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 
-from lancet.system_tray import LancetSystemTray
 from lancet.consts import APP_LOGO_PATH, APP_NAME
+from lancet.system_tray import LancetSystemTray
 
 
 def main() -> None:
--- lancet/consts.py
+++ lancet/consts.py
@@ -1,11 +1,11 @@
-"""
-Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
-License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-"""
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 import pathlib
 
-THIS_DIR = pathlib.Path(__file__).parent
 APP_NAME = "Lancet"
+THIS_DIR = pathlib.Path(__file__).resolve().parent
 APP_LOGO_PATH = THIS_DIR / "icons" / "logo.ico"
 SCREENSHOT_ICON_PATH = THIS_DIR / "icons/screenshot.png"
+OCR_ICON_PATH = THIS_DIR / "icons/ocr.png"
 EXIT_ICON_PATH = THIS_DIR / "icons/exit.png"
