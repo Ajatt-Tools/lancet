ace2a216f84a1a38f291f0795d6fc981003d811f
Ren Tatsumoto <tatsu@autistici.org>
2026-04-23 02:39:38 +0300

    only format lancet

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,7 +6,7 @@ echo "Formatting $PWD"
 
 ROOT=$(git rev-parse --show-toplevel)
 readonly ROOT
-readarray -t FILES <<<"$(find "$ROOT" -type f -iname '*.py')"
+readarray -t FILES <<<"$(find "$ROOT/lancet" -type f -iname '*.py')"
 readonly -a FILES
 
 pyupgrade --py313-plus "${FILES[@]}"
