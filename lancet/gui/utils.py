e68049f41f56f3c0381feb862a59b5fbd35ea2d6
Ren Tatsumoto <tatsu@autistici.org>
2026-04-25 13:13:47 +0300

    make min, max, suffix protected

--- lancet/gui/utils.py
+++ lancet/gui/utils.py
@@ -12,38 +12,38 @@ def ui_translate(key: str) -> str:
 class SecondsSpinBox(QSpinBox):
     """A spin box pre-configured for selecting a duration in seconds."""
 
-    min: int = 1
-    max: int = 120
-    suffix: str = "seconds"
+    _min: int = 1
+    _max: int = 120
+    _suffix: str = "seconds"
 
     def __init__(self, initial_value: int, parent: QWidget | None = None) -> None:
         """Initialize the spin box with range, suffix, and initial value."""
         super().__init__(parent)
-        self.setRange(self.min, self.max)
-        if self.suffix:
-            self.setSuffix(f" {self.suffix}")
+        self.setRange(self._min, self._max)
+        if self._suffix:
+            self.setSuffix(f" {self._suffix}")
         self.setValue(initial_value)
 
 
 class HistorySizeSpinBox(SecondsSpinBox):
     """A spin box for selecting the maximum number of history items."""
 
-    min: int = 1
-    max: int = 1_000
-    suffix: str = "items"
+    _min: int = 1
+    _max: int = 1_000
+    _suffix: str = "items"
 
 
 class BorderThicknessSpinBox(SecondsSpinBox):
     """A spin box for selecting the screenshot overlay border thickness in pixels."""
 
-    min: int = 1
-    max: int = 20
-    suffix: str = "px"
+    _min: int = 1
+    _max: int = 20
+    _suffix: str = "px"
 
 
 class BindPortSpinBox(SecondsSpinBox):
     """A spin box for selecting the screenshot overlay border thickness in pixels."""
 
-    min: int = 1025
-    max: int = 32767
-    suffix: str = ""
+    _min: int = 1025
+    _max: int = 32767
+    _suffix: str = ""
