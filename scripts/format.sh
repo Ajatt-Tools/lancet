b8430c08bf23a02555307a9ecb89b5e8da5711da
Ren Tatsumoto <tatsu@autistici.org>
2026-04-27 11:17:14 +0300

    cd to root

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,6 +6,8 @@ echo "Formatting $PWD"
 
 ROOT=$(git rev-parse --show-toplevel)
 readonly ROOT
+cd -- "$ROOT" || exit 1
+
 readarray -t FILES <<<"$(find "$ROOT/lancet" -type f -iname '*.py')"
 readonly -a FILES
 
