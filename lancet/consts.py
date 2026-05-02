858abf623515d9051fd91a30439db0781e4efe89
Ren Tatsumoto <tatsu@autistici.org>
2026-05-02 22:27:32 +0300

    mark types

--- lancet/consts.py
+++ lancet/consts.py
@@ -3,29 +3,33 @@
 import os
 import pathlib
 import sys
+import typing
 
-APP_NAME = "Lancet"
-THIS_DIR = pathlib.Path(__file__).resolve().parent
-DESKTOP_FILE = THIS_DIR / f"{APP_NAME.lower()}.desktop"
-ICONS_DIR = THIS_DIR / "icons"
-APP_LOGO_PATH = ICONS_DIR / "app_logo.png"
-SCREENSHOT_ICON_PATH = ICONS_DIR / "screenshot.png"
-OCR_ICON_PATH = ICONS_DIR / "ocr.png"
-EXIT_ICON_PATH = ICONS_DIR / "exit.png"
-RESTART_ICON_PATH = ICONS_DIR / "restart.png"
-PREFERENCES_ICON_PATH = ICONS_DIR / "preferences.png"
-CFG_DIR_PATH = pathlib.Path(os.environ.get("XDG_CONFIG_HOME", pathlib.Path.home() / ".config")) / APP_NAME.lower()
-CFG_PATH = CFG_DIR_PATH / f"{APP_NAME.lower()}.json"
+P = typing.Final[pathlib.Path]
+S = typing.Final[str]
 
-CACHE_DIR_PATH = pathlib.Path(os.environ.get("XDG_CACHE_HOME", pathlib.Path.home() / ".cache")) / APP_NAME.lower()
-HISTORY_FILE_PATH = CACHE_DIR_PATH / "ocr_history.json"
-GEOMETRY_FILE_PATH = CACHE_DIR_PATH / "geometry"
+APP_NAME: S = "Lancet"
+THIS_DIR: P = pathlib.Path(__file__).resolve().parent
+DESKTOP_FILE: P = THIS_DIR / f"{APP_NAME.lower()}.desktop"
+ICONS_DIR: P = THIS_DIR / "icons"
+APP_LOGO_PATH: P = ICONS_DIR / "app_logo.png"
+SCREENSHOT_ICON_PATH: P = ICONS_DIR / "screenshot.png"
+OCR_ICON_PATH: P = ICONS_DIR / "ocr.png"
+EXIT_ICON_PATH: P = ICONS_DIR / "exit.png"
+RESTART_ICON_PATH: P = ICONS_DIR / "restart.png"
+PREFERENCES_ICON_PATH: P = ICONS_DIR / "preferences.png"
+CFG_DIR_PATH: P = pathlib.Path(os.environ.get("XDG_CONFIG_HOME", pathlib.Path.home() / ".config")) / APP_NAME.lower()
+CFG_PATH: P = CFG_DIR_PATH / f"{APP_NAME.lower()}.json"
+
+CACHE_DIR_PATH: P = pathlib.Path(os.environ.get("XDG_CACHE_HOME", pathlib.Path.home() / ".cache")) / APP_NAME.lower()
+HISTORY_FILE_PATH: P = CACHE_DIR_PATH / "ocr_history.json"
+GEOMETRY_FILE_PATH: P = CACHE_DIR_PATH / "geometry"
 
 IS_MAC = sys.platform.startswith("darwin")
 IS_WIN = sys.platform.startswith("win32")
 
-GITHUB_URL = "https://github.com/Ajatt-Tools/lancet"
-CHAT_URL = "https://ajatt.top/blog/join-our-community.html"
+GITHUB_URL: S = "https://github.com/Ajatt-Tools/lancet"
+CHAT_URL: S = "https://ajatt.top/blog/join-our-community.html"
 
-DEFAULT_MODEL_NAME = "tatsumoto/manga-ocr-base"
-OCR_JOIN_STR = " "
+DEFAULT_MODEL_NAME: S = "tatsumoto/manga-ocr-base"
+OCR_JOIN_STR: S = " "
