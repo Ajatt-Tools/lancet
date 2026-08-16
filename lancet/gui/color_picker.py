4178308deb2b641610dea30560d57558072073ff
Ren Tatsumoto <tatsu@autistici.org>
2026-08-17 01:43:36 +0300

    add signal color_changed to ColorEditPicker

--- lancet/gui/color_picker.py
+++ lancet/gui/color_picker.py
@@ -1,10 +1,13 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtGui import QColor
 from PyQt6.QtWidgets import QColorDialog, QHBoxLayout, QPushButton, QWidget
-from zala.utils import qconnect
+from zala.utils import q_emit, qconnect
 
 from lancet.gui.line_edit import ColorEdit
+
+DEFAULT_COLOR = "black"
 
 
 def color_to_hex_argb(color: QColor) -> str:
@@ -13,11 +16,13 @@ def color_to_hex_argb(color: QColor) -> str:
 
 
 class ColorEditPicker(QWidget):
+    color_changed = pyqtSignal(str)
+
     def __init__(self, initial_color: str, parent=None) -> None:
         super().__init__(parent)
         # Create members
         self._edit = ColorEdit()
-        self.set_color(initial_color)
+        self.set_color(initial_color or DEFAULT_COLOR)
         # Create layout
         self.setLayout(layout := QHBoxLayout())
         layout.setContentsMargins(0, 0, 0, 0)
@@ -28,10 +33,12 @@ class ColorEditPicker(QWidget):
         # bool checked is passed to the slot.
         # https://doc.qt.io/qt-6/qabstractbutton.html#clicked
         qconnect(b.clicked, lambda: self.choose_color())
+        # https://doc.qt.io/qt-6/qlineedit.html#textChanged
+        qconnect(self._edit.textChanged, lambda text: q_emit(self.color_changed, text))
 
     def choose_color(self) -> None:
         color = QColorDialog.getColor(
-            initial=QColor.fromString(self._edit.text() or "black"),
+            initial=QColor.fromString(self._edit.text() or DEFAULT_COLOR),
             parent=self,
             title="Select color",
             options=QColorDialog.ColorDialogOption.ShowAlphaChannel,
